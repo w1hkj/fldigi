f1e86c8a6a10b46703068602276c35c9100816d1
Stelios Bounanos <vcs@enotty.net>
2008-04-08 22:16:13 +0100

    Fix online help on win32

--- src/dialogs/fl_digi.cxx
+++ src/dialogs/fl_digi.cxx
@@ -30,6 +30,10 @@
 #  include <sys/msg.h>
 #endif
 
+#ifdef __CYGWIN__
+#  include <w32api/windows.h>
+#endif
+
 #include <stdlib.h>
 #include <string>
 
@@ -650,6 +654,7 @@ void cb_mnuAbout(Fl_Widget*, void*)
 void cb_mnuVisitURL(Fl_Widget*, void* arg)
 {
 	const char* url = reinterpret_cast<const char *>(arg);
+#ifndef __CYGWIN__
 	const char* browsers[] = { getenv("BROWSER"), "xdg-open", "sensible-brower",
 				   "firefox", "mozilla" };
 	switch (fork()) {
@@ -664,6 +669,17 @@ void cb_mnuVisitURL(Fl_Widget*, void* arg)
 			 "Open this URL manually:\n%s",
 			 strerror(errno), url);
 	}
+#else
+	// gurgle... gurgle... HOWL
+	// "The return value is cast as an HINSTANCE for backward
+	// compatibility with 16-bit Windows applications. It is
+	// not a true HINSTANCE, however. The only thing that can
+	// be done with the returned HINSTANCE is to cast it to an
+	// int and compare it with the value 32 or one of the error
+	// codes below." (Error codes omitted to preserve sanity).
+	if ((int)ShellExecute(NULL, "open", url, NULL, NULL, SW_SHOWNORMAL) <= 32)
+		fl_alert("Could not open url:\n%s\n", url);
+#endif
 
         restoreFocus();
 }
