ed339d93850bd931cceb65b1a0f3179adb880f57
Stelios Bounanos <vcs@enotty.net>
2008-04-28 20:46:23 +0100

    Use unnamed semaphores Also check for zoh/linear converters by enum name

--- src/soundcard/soundconf.cxx
+++ src/soundcard/soundconf.cxx
@@ -194,8 +194,8 @@ static void sound_init_options(void)
 
 	const char* cname;
 	for (int i = 0; (cname = src_get_name(i)); i++) {
-		if (strstr( cname, "ZOH") != 0) continue;
-		if (strstr( cname, "Linear") != 0) continue;
+		if (i == SRC_ZERO_ORDER_HOLD || i == SRC_LINEAR)
+			continue;
 		menuSampleConverter->add(cname);
 	}
 	menuSampleConverter->value(progdefaults.sample_converter);
