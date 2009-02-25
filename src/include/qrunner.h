7f7f2539156b288118a5cabf58605a5a7094a529
Stelios Bounanos <vcs@enotty.net>
2009-02-25 18:35:07 +0000

    Fix printf format string
    (cherry picked from commit eb1ec8ac81b1720ceeacfbb3635d66ed1fbdb002)

--- src/include/qrunner.h
+++ src/include/qrunner.h
@@ -24,6 +24,7 @@
 #define QRUNNER_H_
 
 #ifndef NDEBUG
+#    include <inttypes.h>
 #    include "debug.h"
 #endif
 
@@ -107,7 +108,7 @@ public:
                 }
 
 #ifndef NDEBUG
-		LOG_ERROR("qrunner: thread %d fifo full!", GET_THREAD_ID());
+		LOG_ERROR("qrunner: thread %" PRIdPTR " fifo full!", GET_THREAD_ID());
 #endif
                 return false;
         }
