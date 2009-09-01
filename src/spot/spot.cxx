w1hkj/fldigi

commit 53a2820a20e697e355759fbef3cbacc552270598

Improve dxcc, spot, pskrep performance
* Use unordered_maps (or hash_maps on ancient gcc4 versions)
* Store the spot callbacks such that RE searching is minimised
* Store list of country names

src/spot/spot.cxx
@@ -22,8 +22,9 @@
 
 #include <config.h>
 
-#include <vector>
-#include <map>
+#include <list>
+#include <tr1/unordered_map>
+#include <functional>
 
 #include "trx.h"
 #include "globals.h"
@@ -32,57 +33,55 @@
 #include "debug.h"
 #include "spot.h"
 
+// the number of characters that we match our REs against
 #define SEARCHLEN 32
 #define DECBUFSIZE 8 * SEARCHLEN
 
 
 using namespace std;
 
-struct callback_t {
+struct callback_t
+{
 	void* data;
-
 	spot_log_cb_t lcb;
-
 	spot_log_cb_t mcb;
-
 	spot_recv_cb_t rcb;
-	fre_t* re;
+};
+typedef list<callback_t> cblist_t;
+
+
+struct fre_hash : std::unary_function<const fre_t*, size_t>
+{
+	size_t operator()(const fre_t* r) const { return r->hash(); }
+};
+struct fre_comp : std::unary_function<const fre_t*, bool>
+{
+	size_t operator()(const fre_t* l, const fre_t* r) const { return *l == *r; }
 };
 
-static map<int, string> buffers;
-typedef vector<callback_t> cblist_t;
+typedef list<callback_t*> callback_p_list_t;
+typedef tr1::unordered_map<fre_t*, callback_p_list_t, fre_hash, fre_comp> rcblist_t;
 
-
+static tr1::unordered_map<int, string> buffers;
 static cblist_t cblist;
+static rcblist_t rcblist;
 
 void spot_recv(char c, int decoder, int afreq)
 {
 	static trx_mode last_mode = NUM_MODES + 1;
 
-	switch (decoder) {
-	case -1: // mode without multiple decoders
+	if (decoder == -1) // mode without multiple decoders
 		decoder = active_modem->get_mode();
-		if (last_mode > NUM_MODES)
-			last_mode = decoder;
-		else if (last_mode != decoder) {
-			buffers.clear();
-			last_mode = decoder;
-		}
-		break;
-	default:
-		if (last_mode > NUM_MODES)
-			last_mode = active_modem->get_mode();
-		else if (last_mode != active_modem->get_mode()) {
-			buffers.clear();
-			last_mode = active_modem->get_mode();
-		}
-		break;
+	if (last_mode != active_modem->get_mode()) {
+		buffers.clear();
+		last_mode = active_modem->get_mode();
 	}
 	if (afreq == 0)
 		afreq = active_modem->get_freq();
 
 	string& buf = buffers[decoder];
-	buf.reserve(DECBUFSIZE);
+	if (unlikely(buf.capacity() < DECBUFSIZE))
+		buf.reserve(DECBUFSIZE);
 
 	buf += c;
 	string::size_type n = buf.length();
@@ -90,13 +89,16 @@ void spot_recv(char c, int decoder, int afreq)
 		buf.erase(0, DECBUFSIZE - SEARCHLEN);
 	const char* search = buf.c_str() + (n > SEARCHLEN ? n - SEARCHLEN : 0);
 
-	for (cblist_t::iterator i = cblist.begin(); i != cblist.end(); ++i) {
-		if (i->rcb && unlikely(i->re->match(search))) {
-			const vector<regmatch_t>& m = i->re->suboff();
-			if (m.empty())
-				i->rcb(afreq, search, NULL, 0, i->data);
-			else
-				i->rcb(afreq, search, &m[0], m.size(), i->data);
+	for (rcblist_t::iterator i = rcblist.begin(); i != rcblist.end(); ++i) {
+		if (unlikely(i->first->match(search))) {
+			const vector<regmatch_t>& m = i->first->suboff();
+			for (list<callback_t*>::iterator j = i->second.begin();
+			     j != i->second.end() && (*j)->rcb; ++j) {
+				if (m.empty())
+					(*j)->rcb(afreq, search, NULL, 0, (*j)->data);
+				else
+					(*j)->rcb(afreq, search, &m[0], m.size(), (*j)->data);
+			}
 		}
 	}
 }
@@ -121,17 +123,15 @@ static void get_log_details(long long& freq, trx_mode& mode, time_t& rtime)
 void spot_log(const char* callsign, const char* locator, long long freq, trx_mode mode, time_t rtime)
 {
 	get_log_details(freq, mode, rtime);
-	for (cblist_t::const_iterator i = cblist.begin(); i != cblist.end(); ++i)
-		if (i->lcb)
-			i->lcb(callsign, locator, freq, mode, rtime, i->data);
+	for (cblist_t::const_iterator i = cblist.begin(); i != cblist.end() && i->lcb; ++i)
+		i->lcb(callsign, locator, freq, mode, rtime, i->data);
 }
 
 void spot_manual(const char* callsign, const char* locator, long long freq, trx_mode mode, time_t rtime)
 {
 	get_log_details(freq, mode, rtime);
-	for (cblist_t::const_iterator i = cblist.begin(); i != cblist.end(); ++i)
-		if (i->mcb)
-			i->mcb(callsign, locator, freq, mode, rtime, i->data);
+	for (cblist_t::const_iterator i = cblist.begin(); i != cblist.end() && i->mcb; ++i)
+		i->mcb(callsign, locator, freq, mode, rtime, i->data);
 }
 
 //
@@ -140,7 +140,7 @@ void spot_manual(const char* callsign, const char* locator, long long freq, trx_
 //
 void spot_register_log(spot_log_cb_t lcb, void* ldata)
 {
-	callback_t c = { ldata, lcb, 0, 0, 0 };
+	callback_t c = { ldata, lcb, 0, 0 };
 	cblist.push_back(c);
 }
 //
@@ -149,7 +149,7 @@ void spot_register_log(spot_log_cb_t lcb, void* ldata)
 //
 void spot_register_manual(spot_log_cb_t mcb, void* mdata)
 {
-	callback_t c = { mdata, 0, mcb, 0, 0 };
+	callback_t c = { mdata, 0, mcb, 0 };
 	cblist.push_back(c);
 }
 
@@ -164,41 +164,68 @@ void spot_register_manual(spot_log_cb_t mcb, void* mdata)
 //
 void spot_register_recv(spot_recv_cb_t rcb, void* rdata, const char* re, int reflags)
 {
-	callback_t c = { rdata, 0, 0, rcb, new fre_t(re, reflags) };
+	callback_t c = { rdata, 0, 0, rcb };
 	cblist.push_back(c);
+
+	fre_t* fre = new fre_t(re, reflags);
+	rcblist_t::iterator i = rcblist.find(fre);
+	if (i != rcblist.end()) {
+		i->second.push_back(&cblist.back());
+		delete fre;
+	}
+	else
+		rcblist[fre].push_back(&cblist.back());
 	show_spot(true);
 }
 
-void spot_unregister_log(spot_log_cb_t lcb, void* ldata)
+void spot_unregister_log(spot_log_cb_t lcb, const void* ldata)
 {
-	for (cblist_t::reverse_iterator ri = cblist.rbegin(); ri != cblist.rend(); ++ri) {
-		if (lcb == ri->lcb && ldata == ri->data) {
-			cblist.erase((++ri).base());
+	for (cblist_t::iterator i = cblist.begin(); i != cblist.end(); ++i) {
+		if (lcb == i->lcb && ldata == i->data) {
+			cblist.erase(i);
 			break;
 		}
 	}
 }
-void spot_unregister_manual(spot_log_cb_t mcb, void* mdata)
+void spot_unregister_manual(spot_log_cb_t mcb, const void* mdata)
 {
-	for (cblist_t::reverse_iterator ri = cblist.rbegin(); ri != cblist.rend(); ++ri) {
-		if (mcb == ri->mcb && mdata == ri->data) {
-			cblist.erase((++ri).base());
+	for (cblist_t::iterator i = cblist.begin(); i != cblist.end(); ++i) {
+		if (mcb == i->mcb && mdata == i->data) {
+			cblist.erase(i);
 			break;
 		}
 	}
 }
-void spot_unregister_recv(spot_recv_cb_t rcb, void* rdata)
+void spot_unregister_recv(spot_recv_cb_t rcb, const void* rdata)
 {
-	cblist_t::reverse_iterator ri;
-	for (ri = cblist.rbegin(); ri != cblist.rend(); ++ri) {
-		if (rcb == ri->rcb && rdata == ri->data) {
-			delete ri->re;
-			cblist.erase((++ri).base());
+	cblist_t::iterator i;
+	callback_t* p = 0;
+	for (i = cblist.begin(); i != cblist.end(); ++i) {
+		if (rcb == i->rcb && rdata == i->data) {
+			p = &*i;
+			cblist.erase(i);
 			break;
+		}
+	}
+	if (!p)
+		return;
+
+	// remove pointer from rcblist
+	for (rcblist_t::iterator j = rcblist.begin(); j != rcblist.end(); ++j) {
+		for (list<callback_t*>::iterator k = j->second.begin(); k != j->second.end(); ++k) {
+			if (*k == p) {
+				j->second.erase(k);
+				if (j->second.empty()) {
+					delete j->first;
+					rcblist.erase(j);
+				}
+				goto out;
+			}
 		}
 	}
 
-	for (ri = cblist.rbegin(); ri != cblist.rend(); ++ri)
-		if (ri->rcb) break;
-	show_spot(ri != cblist.rend());
+out:
+	for (i = cblist.begin(); i != cblist.end(); ++i)
+		if (i->rcb) break;
+	show_spot(i != cblist.end());
 }

src/include/dxcc.h
@@ -1,6 +1,9 @@
 #ifndef DXCC_H_
 #define DXCC_H_
 
+#include <string>
+#include <vector>
+
 struct dxcc {
 	const char* country;
 	int cq_zone;
@@ -19,6 +22,7 @@ extern const char* qsl_names[];
 bool dxcc_open(const char* filename);
 void dxcc_close(void);
 const dxcc* dxcc_lookup(const char* callsign);
+const std::vector<std::string>* dxcc_entity_list(void);
 
 bool qsl_open(const char* filename, qsl_t qsl_type);
 void qsl_close(void);

src/include/spot.h
@@ -45,8 +45,8 @@ void spot_register_log(spot_log_cb_t lcb, void* ldata);
 void spot_register_manual(spot_log_cb_t mcb, void* mdata);
 void spot_register_recv(spot_recv_cb_t rcb, void* rdata, const char* re, int reflags);
 
-void spot_unregister_log(spot_log_cb_t lcb, void* ldata);
-void spot_unregister_manual(spot_log_cb_t mcb, void* mdata);
-void spot_unregister_recv(spot_recv_cb_t rcb, void* rdata);
+void spot_unregister_log(spot_log_cb_t lcb, const void* ldata);
+void spot_unregister_manual(spot_log_cb_t mcb, const void* mdata);
+void spot_unregister_recv(spot_recv_cb_t rcb, const void* rdata);
 
 #endif // SPOT_H_

src/misc/dxcc.cxx
@@ -8,12 +8,14 @@
 #include <string>
 #include <list>
 #include <map>
+#include <tr1/unordered_map>
 #include <algorithm>
 
 #include "dxcc.h"
 #include "debug.h"
 
 using namespace std;
+using tr1::unordered_map;
 
 
 dxcc::dxcc(const char* cn, int cq, int itu, const char* ct, float lat, float lon, float tz)
@@ -26,9 +28,9 @@ dxcc::dxcc(const char* cn, int cq, int itu, const char* ct, float lat, float lon
 	continent[2] = '\0';
 }
 
-typedef map<string, dxcc*> dxcc_map;
-static dxcc_map* cmap = 0;
-static list<string>* cnames = 0;
+typedef unordered_map<string, dxcc*> dxcc_map_t;
+static dxcc_map_t* cmap = 0;
+static vector<string>* cnames = 0;
 
 static void add_prefix(string& prefix, dxcc* entry);
 
@@ -42,8 +44,9 @@ bool dxcc_open(const char* filename)
 		return false;
 	}
 
-	cmap = new dxcc_map;
-	cnames = new list<string>;
+	cmap = new dxcc_map_t;
+	cnames = new vector<string>;
+	cnames->reserve(345); // approximate number of dxcc entities
 
 	dxcc* entry;
 	string record;
@@ -99,11 +102,16 @@ void dxcc_close(void)
 	delete cnames;
 	cnames = 0;
 	map<dxcc*, bool> rm;
-	for (dxcc_map::iterator i = cmap->begin(); i != cmap->end(); ++i)
+	for (dxcc_map_t::iterator i = cmap->begin(); i != cmap->end(); ++i)
 		if (rm.insert(make_pair(i->second, true)).second)
 			delete i->second;
 	delete cmap;
 	cmap = 0;
+}
+
+const vector<string>* dxcc_entity_list(void)
+{
+	return cnames;
 }
 
 const dxcc* dxcc_lookup(const char* callsign)
@@ -115,11 +123,10 @@ const dxcc* dxcc_lookup(const char* callsign)
 	sstr.resize(strlen(callsign) + 1);
 	transform(callsign, callsign + sstr.length() - 1, sstr.begin() + 1, static_cast<int (*)(int)>(toupper));
 
-	dxcc_map::const_iterator entry;
-
 	// first look for a full callsign (prefixed with '=')
 	sstr[0] = '=';
-	if ((entry = cmap->find(sstr)) != cmap->end())
+	dxcc_map_t::const_iterator entry = cmap->find(sstr);
+	if (entry != cmap->end())
 		return entry->second;
 
 	// erase the '=' and do a longest prefix search
@@ -136,11 +143,9 @@ const dxcc* dxcc_lookup(const char* callsign)
 
 static void add_prefix(string& prefix, dxcc* entry)
 {
-	static dxcc_map::iterator prev_entry = cmap->begin();
-
 	string::size_type i = prefix.find_first_of("([<{");
 	if (likely(i == string::npos)) {
-		prev_entry = cmap->insert(prev_entry, make_pair(prefix, entry));
+		(*cmap)[prefix] = entry;
 		return;
 	}
 
@@ -189,11 +194,11 @@ static void add_prefix(string& prefix, dxcc* entry)
 	} while ((i = prefix.find_first_of("([<{", j)) != string::npos);
 
 	prefix.erase(first);
-	prev_entry = cmap->insert(prev_entry, make_pair(prefix, entry));
+	(*cmap)[prefix] = entry;
 }
 
-typedef map<string, unsigned char> qsl_map;
-static qsl_map* qsl_calls;
+typedef unordered_map<string, unsigned char> qsl_map_t;
+static qsl_map_t* qsl_calls;
 const char* qsl_names[] = { "LoTW", "eQSL" };
 
 bool qsl_open(const char* filename, qsl_t qsl_type)
@@ -202,18 +207,16 @@ bool qsl_open(const char* filename, qsl_t qsl_type)
 	if (!in)
 		return false;
 	if (!qsl_calls)
-		qsl_calls = new qsl_map;
+		qsl_calls = new qsl_map_t;
 
 	size_t n = qsl_calls->size();
-	qsl_map::iterator prev_entry = qsl_calls->begin();
 	string::size_type p;
 	string s;
 	s.reserve(32);
 	while (getline(in, s)) {
 		if ((p = s.rfind('\r')) != string::npos)
 			s.erase(p);
-		prev_entry = qsl_calls->insert(prev_entry, make_pair(s, 0));
-		prev_entry->second |= (1 << qsl_type);
+		(*qsl_calls)[s] |= (1 << qsl_type);
 	}
 
 	LOG_INFO("Added %" PRIuSZ " %s callsigns from \"%s\"",
@@ -237,6 +240,6 @@ unsigned char qsl_lookup(const char* callsign)
 	str.resize(strlen(callsign));
 	transform(callsign, callsign + str.length(), str.begin(), static_cast<int (*)(int)>(toupper));
 
-	qsl_map::const_iterator i = qsl_calls->find(str);
+	qsl_map_t::const_iterator i = qsl_calls->find(str);
 	return i == qsl_calls->end() ? 0 : i->second;
 }

src/spot/pskrep.cxx
@@ -45,11 +45,29 @@
 #include <cstdlib>
 
 #include <string>
-#include <map>
 #include <deque>
 #include <vector>
 #include <algorithm>
 #include <fstream>
+
+#if (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1)
+#  define MAP_TYPE std::tr1::unordered_map
+#define HASH_TYPE std::tr1::hash
+#  include <tr1/unordered_map>
+#else
+// use the non-standard gnu hash_map on gcc <= 4.0.x,
+// which has a broken tr1::unordered_map::operator=
+#  define MAP_TYPE __gnu_cxx::hash_map
+#define HASH_TYPE __gnu_cxx::hash
+#  include <ext/hash_map>
+namespace __gnu_cxx {
+	// define the missing hash specialisation for std::string
+	// using the 'const char*' hash function
+	template<> struct hash<std::string> {
+		size_t operator()(const std::string& s) const { return __stl_hash_string(s.c_str()); }
+	};
+}
+#endif
 
 #include <FL/Fl.H>
 
@@ -117,10 +135,9 @@ struct rcpt_report_t
 // A band_map_t holds a list of reception reports (for a particular callsign and band)
 typedef deque<rcpt_report_t> band_map_t;
 // A call_map_t holds reception reports for a particular callsign
-typedef map<band_t, band_map_t> call_map_t;
+typedef MAP_TYPE<band_t, band_map_t, HASH_TYPE<int> > call_map_t;
 // A container of this type holds all reception reports, sorted by callsign and band
-typedef map<string, call_map_t> queue_t;
-
+typedef MAP_TYPE<string, call_map_t> queue_t;
 
 class pskrep_sender
 {

commit 78d87d74481a6c855eb47243db35bdd6051982bc

Fix log/manual spotting
Remarkably stupid breakage introduced by commit
53a2820a20e697e355759fbef3cbacc552270598.

src/spot/spot.cxx
@@ -123,15 +123,17 @@ static void get_log_details(long long& freq, trx_mode& mode, time_t& rtime)
 void spot_log(const char* callsign, const char* locator, long long freq, trx_mode mode, time_t rtime)
 {
 	get_log_details(freq, mode, rtime);
-	for (cblist_t::const_iterator i = cblist.begin(); i != cblist.end() && i->lcb; ++i)
-		i->lcb(callsign, locator, freq, mode, rtime, i->data);
+	for (cblist_t::const_iterator i = cblist.begin(); i != cblist.end(); ++i)
+		if (i->lcb)
+			i->lcb(callsign, locator, freq, mode, rtime, i->data);
 }
 
 void spot_manual(const char* callsign, const char* locator, long long freq, trx_mode mode, time_t rtime)
 {
 	get_log_details(freq, mode, rtime);
-	for (cblist_t::const_iterator i = cblist.begin(); i != cblist.end() && i->mcb; ++i)
-		i->mcb(callsign, locator, freq, mode, rtime, i->data);
+	for (cblist_t::const_iterator i = cblist.begin(); i != cblist.end(); ++i)
+		if (i->mcb)
+			i->mcb(callsign, locator, freq, mode, rtime, i->data);
 }
 
 //