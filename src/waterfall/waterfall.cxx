w1hkj/fldigi

commit 65f70cd1ff8e1d6065be36a4012436bb4f39ac4c

WF Limits
    * Added limit controls on waterfall cursor movement/selection
    * Added limit controls on modem AFC movement
    * Limits imposed by LowFreqCutoff, HighFreqCutoff config items
      Effect immediate to configuration item change
    * Fixed bug which caused erroneous request to save log on exit

src/waterfall/waterfall.cxx
@@ -112,8 +112,8 @@ short int *tmp_fft_db;
 WFdisp::WFdisp (int x0, int y0, int w0, int h0, char *lbl) :
 			  Fl_Widget(x0,y0,w0,h0,"") {
 	disp_width = w();
-	if (disp_width > progdefaults.wfwidth/4)
-		disp_width = progdefaults.wfwidth/4;
+	if (disp_width > progdefaults.HighFreqCutoff/4)
+		disp_width = progdefaults.HighFreqCutoff/4;
 	scale_width = IMAGE_WIDTH * 2;
 	image_height = h() - WFTEXT - WFSCALE - WFMARKER;
 	image_area      = IMAGE_WIDTH * image_height;
@@ -253,16 +253,16 @@ void WFdisp::makeMarker()
 
 	if (!wantcursor) return;
 
-	if (cursorpos > progdefaults.wfwidth - bandwidth / 2 / step)
-		cursorpos = progdefaults.wfwidth - bandwidth / 2 / step;
-	if (cursorpos >= (progdefaults.wfwidth - offset - bandwidth/2)/step)
-		cursorpos = (progdefaults.wfwidth - offset - bandwidth/2)/step - 1;
-	if (cursorpos < bandwidth / 2 / step)
-		cursorpos = bandwidth / 2 / step + 1;
+	if (cursorpos > progdefaults.HighFreqCutoff - bandwidth / 2 / step)
+		cursorpos = progdefaults.HighFreqCutoff - bandwidth / 2 / step;
+	if (cursorpos >= (progdefaults.HighFreqCutoff - offset - bandwidth/2)/step)
+		cursorpos = (progdefaults.HighFreqCutoff - offset - bandwidth/2)/step;
+	if (cursorpos < (progdefaults.LowFreqCutoff + bandwidth / 2) / step)
+		cursorpos = (progdefaults.LowFreqCutoff + bandwidth / 2) / step;
 
 // Create the cursor marker
 	double xp = offset + step * cursorpos;
-	if (xp < bandwidth / 2.0 || xp > (progdefaults.wfwidth - bandwidth / 2.0))
+	if (xp < bandwidth / 2.0 || xp > (progdefaults.HighFreqCutoff - bandwidth / 2.0))
 		return;
 	clrM = markerimage + scale_width + (int)(xp + 0.5);
 	RGBcursor.R = progdefaults.cursorLineRGBI.R;
@@ -564,8 +564,8 @@ void WFdisp::checkoffset() {
 		if (sigoffset > (IMAGE_WIDTH - disp_width))
 			sigoffset = IMAGE_WIDTH - disp_width;
 	} else {
-		if (offset > (int)(progdefaults.wfwidth - step * disp_width))
-			offset = (int)(progdefaults.wfwidth - step * disp_width);
+		if (offset > (int)(progdefaults.HighFreqCutoff - step * disp_width))
+			offset = (int)(progdefaults.HighFreqCutoff - step * disp_width);
 		if (offset < 0)
 			offset = 0;
 	}
@@ -603,11 +603,11 @@ void WFdisp::checkWidth()
 {
 	disp_width = w();
 	if (mag == MAG_1) step = 4;
-	if (mag == MAG_1 && disp_width > progdefaults.wfwidth/4)
-		disp_width = progdefaults.wfwidth/4;
+	if (mag == MAG_1 && disp_width > progdefaults.HighFreqCutoff/4)
+		disp_width = progdefaults.HighFreqCutoff/4;
 	if (mag == MAG_2) step = 2;
-	if (mag == MAG_2 && disp_width > progdefaults.wfwidth/2)
-		disp_width = progdefaults.wfwidth/2;
+	if (mag == MAG_2 && disp_width > progdefaults.HighFreqCutoff/2)
+		disp_width = progdefaults.HighFreqCutoff/2;
 	if (mag == MAG_4) step = 1;
 }
 
@@ -970,6 +970,7 @@ void carrier_cb(Fl_Widget *w, void *v) {
 	Fl_Counter *cntr = (Fl_Counter *)w;
 	waterfall *wf = (waterfall *)w->parent();
 	int selfreq = (int) cntr->value();
+	if (selfreq > progdefaults.HighFreqCutoff) selfreq = progdefaults.HighFreqCutoff - wf->wfdisp->Bandwidth() / 2;
 	stopMacroTimer();
 	active_modem->set_freq(selfreq);
 	wf->wfdisp->carrier(selfreq);
@@ -1228,13 +1229,14 @@ void btnMem_cb(Fl_Widget *, void *menu_event)
 
 void waterfall::opmode() {
 	int val = (int)active_modem->get_bandwidth();
-	if (wfdisp->carrier() < val/2)
-		wfdisp->carrier( val/2 );
-	if (wfdisp->carrier() > IMAGE_WIDTH - val/2)
-		wfdisp->carrier( IMAGE_WIDTH - val/2);
+	
+	wfdisp->carrier((int)CLAMP(
+		wfdisp->carrier(), 
+		progdefaults.LowFreqCutoff + val / 2 + 1, 
+		progdefaults.HighFreqCutoff - val / 2) - 1);
 	wfdisp->Bandwidth( val );
 	FL_LOCK_D();
-	wfcarrier->range(val/2, IMAGE_WIDTH - val/2-1);
+	wfcarrier->range(progdefaults.LowFreqCutoff + val/2, progdefaults.HighFreqCutoff - val/2 - 1);
 	FL_UNLOCK_D();
 }
 
@@ -1432,7 +1434,7 @@ waterfall::waterfall(int x0, int y0, int w0, int h0, char *lbl) :
 	wfcarrier->step(1.0);
 	wfcarrier->lstep(10.0);
 	wfcarrier->precision(0);
-	wfcarrier->range(16.0, IMAGE_WIDTH - 16.0);
+	wfcarrier->range(16.0, progdefaults.HighFreqCutoff - 16.0);
 	wfcarrier->value(wfdisp->carrier());
 	wfcarrier->tooltip(_("Adjust cursor frequency"));
 
@@ -1722,7 +1724,10 @@ int WFdisp::handle(int event)
 			if (progdefaults.WaterfallHistoryDefault)
 				bHistory = true;
 			newcarrier = cursorFreq(xpos);
-			newcarrier = (int)CLAMP(newcarrier, wf->wfcarrier->minimum(), wf->wfcarrier->maximum());
+			newcarrier = (int)CLAMP(
+				newcarrier, 
+				progdefaults.LowFreqCutoff + active_modem->get_bandwidth() / 2, 
+				progdefaults.HighFreqCutoff - active_modem->get_bandwidth() / 2);
 			active_modem->set_freq(newcarrier);
 			if (!(Fl::event_state() & FL_SHIFT))
 				active_modem->set_sigsearch(SIGSEARCH);
@@ -1802,7 +1807,10 @@ int WFdisp::handle(int event)
 		case FL_Left: case FL_Right:
 			if (k == FL_Left)
 				d = -d;
-			oldcarrier = newcarrier = (int)CLAMP(carrier() + d, wf->wfcarrier->minimum(), wf->wfcarrier->maximum());
+			oldcarrier = newcarrier = (int)CLAMP(
+				carrier() + d, 
+				progdefaults.LowFreqCutoff + active_modem->get_bandwidth() / 2, 
+				progdefaults.HighFreqCutoff - active_modem->get_bandwidth() / 2);
 			active_modem->set_freq(newcarrier);
 			redrawCursor();
 			break;

src/dialogs/confdialog.cxx
@@ -580,6 +580,7 @@ Fl_Counter *cntLowFreqCutoff=(Fl_Counter *)0;
 static void cb_cntLowFreqCutoff(Fl_Counter* o, void*) {
   progdefaults.LowFreqCutoff=(int)(o->value());
 progdefaults.changed = true;
+setwfrange();
 }
 
 Fl_Counter *valLatency=(Fl_Counter *)0;
@@ -606,8 +607,9 @@ progdefaults.changed = true;
 Fl_Counter *cntrWfwidth=(Fl_Counter *)0;
 
 static void cb_cntrWfwidth(Fl_Counter* o, void*) {
-  progdefaults.wfwidth = (int)o->value();
+  progdefaults.HighFreqCutoff = (int)o->value();
 progdefaults.changed = true;
+setwfrange();
 }
 
 Fl_Counter *cntrWfheight=(Fl_Counter *)0;
@@ -2903,7 +2905,7 @@ static const char szBaudRates[] = "300|600|1200|2400|4800|9600|19200|38400|57600
             { Fl_Group* o = new Fl_Group(5, 62, 490, 135);
               o->box(FL_ENGRAVED_FRAME);
               { Fl_Counter* o = cntLowFreqCutoff = new Fl_Counter(50, 72, 70, 20, _("Lower limit"));
-                cntLowFreqCutoff->tooltip(_("Suppress waterfall below this audio frequency"));
+                cntLowFreqCutoff->tooltip(_("Low frequency limit in Hz"));
                 cntLowFreqCutoff->type(1);
                 cntLowFreqCutoff->minimum(0);
                 cntLowFreqCutoff->maximum(500);
@@ -2942,7 +2944,7 @@ an merging"));
                 mnuFFTPrefilter->value(progdefaults.wfPreFilter);
               } // Fl_Choice* mnuFFTPrefilter
               { Fl_Counter* o = cntrWfwidth = new Fl_Counter(273, 72, 95, 21, _("Upper limit"));
-                cntrWfwidth->tooltip(_("CPU usage increases with waterfall height"));
+                cntrWfwidth->tooltip(_("High frequency limit in Hz"));
                 cntrWfwidth->type(1);
                 cntrWfwidth->minimum(2000);
                 cntrWfwidth->maximum(4000);
@@ -2950,7 +2952,7 @@ an merging"));
                 cntrWfwidth->value(3000);
                 cntrWfwidth->callback((Fl_Callback*)cb_cntrWfwidth);
                 cntrWfwidth->align(FL_ALIGN_RIGHT);
-                o->value(progdefaults.wfwidth);
+                o->value(progdefaults.HighFreqCutoff);
               } // Fl_Counter* cntrWfwidth
               o->end();
             } // Fl_Group* o

src/dialogs/confdialog.fl
@@ -725,8 +725,9 @@ progdefaults.changed = true;}
               Fl_Counter cntLowFreqCutoff {
                 label {Lower limit}
                 callback {progdefaults.LowFreqCutoff=(int)(o->value());
-progdefaults.changed = true;}
-                tooltip {Suppress waterfall below this audio frequency} xywh {50 72 70 20} type Simple align 8 minimum 0 maximum 500 step 50 value 300
+progdefaults.changed = true;
+setwfrange();}
+                tooltip {Low frequency limit in Hz} xywh {50 72 70 20} type Simple align 8 minimum 0 maximum 500 step 50 value 300
                 code0 {o->value(progdefaults.LowFreqCutoff);}
               }
               Fl_Counter valLatency {
@@ -756,10 +757,11 @@ progdefaults.changed = true;} open
               } {}
               Fl_Counter cntrWfwidth {
                 label {Upper limit}
-                callback {progdefaults.wfwidth = (int)o->value();
-progdefaults.changed = true;}
-                tooltip {CPU usage increases with waterfall height} xywh {273 72 95 21} type Simple align 8 minimum 2000 maximum 4000 step 100 value 3000
-                code0 {o->value(progdefaults.wfwidth);}
+                callback {progdefaults.HighFreqCutoff = (int)o->value();
+progdefaults.changed = true;
+setwfrange();}
+                tooltip {High frequency limit in Hz} xywh {273 72 95 21} type Simple align 8 minimum 2000 maximum 4000 step 100 value 3000
+                code0 {o->value(progdefaults.HighFreqCutoff);}
               }
             }
             Fl_Group {} {

src/dialogs/fl_digi.cxx
@@ -1516,7 +1516,7 @@ void cb_call(Fl_Widget* w, void*)
 	if (progStatus.timer && (Fl::event() != FL_HIDE))
 		stopMacroTimer();
 
-	if (old_call == new_call)
+	if (old_call == new_call || new_call.empty())
 		return restoreFocus(w);
 
 	old_call = new_call;
@@ -2492,6 +2492,10 @@ void showMacroSet() {
 	}
 }
 
+void setwfrange() {
+	wf->opmode();
+}
+
 void create_fl_digi_main() {
 
 	int fnt = fl_font();
@@ -2506,7 +2510,7 @@ void create_fl_digi_main() {
 
 	x_qsoframe += rig_control_width;
 
-	IMAGE_WIDTH = 4000;//progdefaults.wfwidth;
+	IMAGE_WIDTH = 4000;//progdefaults.HighFreqCutoff;
 	Hwfall = progdefaults.wfheight;
 
 	Wwfall = progStatus.mainW - 2 * DEFAULT_SW;

src/include/configuration.h
@@ -107,6 +107,9 @@
         ELEM_(int, LowFreqCutoff, "LOWFREQCUTOFF",                                      \
               "Lowest frequency shown on waterfall (Hz)",                               \
               0)                                                                        \
+        ELEM_(int,  HighFreqCutoff, "HIGHFREQCUTOFF",                                   \
+              "Highest frequency shown on waterfall (Hz)",                              \
+              3000)                                                                     \
         ELEM_(double, CWsweetspot, "CWSWEETSPOT",                                       \
               "Default CW tracking point (Hz)",                                         \
               1000)                                                                     \
@@ -469,9 +472,6 @@
         ELEM_(bool, docked_rig_control, "DOCKEDRIGCONTROL",                             \
               "Docked rig control",                                                     \
               true)                                                                     \
-        ELEM_(int,  wfwidth, "WFWIDTH",                                                 \
-              "Waterfall width (Hz)",                                                   \
-              3000)                                                                     \
         ELEM_(int,  wfheight, "WFHEIGHT",                                               \
               "Waterfall height (pixels)",                                              \
               125)                                                                      \

src/include/fl_digi.h
@@ -170,6 +170,7 @@ extern void show_mode(const std::string& mode);
 extern void show_bw(const std::string& sWidth);
 extern void show_spot(bool v);
 extern void showMacroSet();
+extern void setwfrange();
 
 extern void put_WARNstatus(double);
 

src/main.cxx
@@ -555,7 +555,7 @@ int parse_args(int argc, char **argv, int& idx)
 	       OPT_BENCHMARK_SRC_RATIO, OPT_BENCHMARK_SRC_TYPE,
 #endif
 
-               OPT_FONT, OPT_WFALL_HEIGHT, OPT_WFALL_WIDTH,
+               OPT_FONT, OPT_WFALL_HEIGHT,
                OPT_WINDOW_WIDTH, OPT_WINDOW_HEIGHT,
 //               OPT_TOGGLE_CHECK,
 #if USE_PORTAUDIO
@@ -600,7 +600,6 @@ int parse_args(int argc, char **argv, int& idx)
 
 		{ "font",	   1, 0, OPT_FONT },
 
-		{ "wfall-width",   1, 0, OPT_WFALL_WIDTH },
 		{ "wfall-height",  1, 0, OPT_WFALL_HEIGHT },
 		{ "window-width",  1, 0, OPT_WINDOW_WIDTH },
 		{ "window-height", 1, 0, OPT_WINDOW_HEIGHT },
@@ -740,10 +739,6 @@ int parse_args(int argc, char **argv, int& idx)
 			}
 		}
 			Fl::set_font(FL_HELVETICA, optarg);
-			break;
-
-		case OPT_WFALL_WIDTH:
-			progdefaults.wfwidth = strtol(optarg, NULL, 10);
 			break;
 
 		case OPT_WFALL_HEIGHT:

src/trx/modem.cxx
@@ -131,8 +131,10 @@ void modem::init()
 
 void modem::set_freq(double freq)
 {
-	frequency = freq;
-//	freqerr = 0.0;
+	frequency = CLAMP(
+		freq, 
+		progdefaults.LowFreqCutoff + bandwidth / 2 + 1, 
+		progdefaults.HighFreqCutoff - bandwidth / 2 - 1);
 	if (freqlock == false)
 		tx_frequency = frequency;
 	REQ(put_freq, frequency);

commit 75be29e6be11977d8abd89ac3a98cddbad858abe

Debug code removal
    * Removed debugging code from waterfall.cxx
    * Used to create
      commit ddc5790b7b70a96b579011ac77919cfa157754bc
      which corrected bug introduced in
      commit 65f70cd1ff8e1d6065be36a4012436bb4f39ac4c

src/waterfall/waterfall.cxx
@@ -588,7 +588,6 @@ void WFdisp::movetocenter() {
 }
 
 void WFdisp::carrier(int cf) {
-printf("carrier = %d\n", cf);
 	if (cf >= bandwidth / 2 && cf < (IMAGE_WIDTH - bandwidth / 2)) {
 		carrierfreq = cf;
 		makeMarker();
@@ -974,7 +973,6 @@ void carrier_cb(Fl_Widget *w, void *v) {
 	if (selfreq > progdefaults.HighFreqCutoff) selfreq = progdefaults.HighFreqCutoff - wf->wfdisp->Bandwidth() / 2;
 	stopMacroTimer();
 	active_modem->set_freq(selfreq);
-printf("selfreq %d\n", selfreq);
 	wf->wfdisp->carrier(selfreq);
 	restoreFocus();
 }