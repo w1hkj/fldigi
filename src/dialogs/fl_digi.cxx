w1hkj/fldigi

commit bb37cc1c6ba86c3b507a0710784715223fb59002

Fix -iconic option handling
  * Hide all windows if the -i/-iconic option was passed
  * Use Fl_Window::show(int, char**) to show the icon window (X11)

src/dialogs/fl_digi.cxx
@@ -3632,19 +3632,6 @@ void create_fl_digi_main_primary() {
 	fl_digi_main->end();
 	fl_digi_main->callback(cb_wMain);
 
-#if defined(__WOE32__)
-#  ifndef IDI_ICON
-#    define IDI_ICON 101
-#  endif
-	fl_digi_main->icon((char*)LoadIcon(fl_display, MAKEINTRESOURCE(IDI_ICON)));
-#elif !defined(__APPLE__) && USE_X
-	make_pixmap(&fldigi_icon_pixmap, fldigi_icon);
-	fl_digi_main->icon((char *)fldigi_icon_pixmap);
-#endif
-
-	fl_digi_main->xclass(PACKAGE_NAME);
-	fl_digi_main->size_range(WMIN, HMIN);
-
 	scopeview = new Fl_Double_Window(0,0,140,140, _("Scope"));
 	scopeview->xclass(PACKAGE_NAME);
 	digiscope = new Digiscope (0, 0, 140, 140);
@@ -4130,21 +4117,6 @@ void create_fl_digi_main_WF_only() {
 			}
 		}
 	}
-//---------------------------
-#if defined(__WOE32__)
-#  ifndef IDI_ICON
-#	define IDI_ICON 101
-#  endif
-	fl_digi_main->icon((char*)LoadIcon(fl_display, MAKEINTRESOURCE(IDI_ICON)));
-#elif !defined(__APPLE__) && USE_X
-	make_pixmap(&fldigi_icon_pixmap, fldigi_icon);
-	fl_digi_main->icon((char *)fldigi_icon_pixmap);
-#endif
-
-	fl_digi_main->xclass(PACKAGE_NAME);
-	fl_digi_main->size_range(WMIN, nomH, 0, nomH);
-
-//--------------------------
 
 	make_scopeviewer();
 	noop_controls();
@@ -4170,11 +4142,25 @@ void create_fl_digi_main_WF_only() {
 }
 
 
-void create_fl_digi_main() {
+void create_fl_digi_main(int argc, char** argv)
+{
 	if (bWF_only)
 		create_fl_digi_main_WF_only();
 	else
 		create_fl_digi_main_primary();
+
+#if defined(__WOE32__)
+#  ifndef IDI_ICON
+#    define IDI_ICON 101
+#  endif
+	fl_digi_main->icon((char*)LoadIcon(fl_display, MAKEINTRESOURCE(IDI_ICON)));
+#elif !defined(__APPLE__) && USE_X
+	make_pixmap(&fldigi_icon_pixmap, fldigi_icon, argc, argv);
+	fl_digi_main->icon((char *)fldigi_icon_pixmap);
+#endif
+
+	fl_digi_main->xclass(PACKAGE_NAME);
+	fl_digi_main->size_range(WMIN, HMIN);
 }
 
 void put_freq(double frequency)

src/flarq-src/flarq.cxx
@@ -1823,7 +1823,7 @@ int main (int argc, char *argv[] )
 #  endif
 	arqwin->icon((char*)LoadIcon(fl_display, MAKEINTRESOURCE(IDI_ICON)));
 #elif !defined(__APPLE__) && USE_X
-	make_pixmap(&flarq_icon_pixmap, flarq_icon);
+	make_pixmap(&flarq_icon_pixmap, flarq_icon, argc, argv);
 	arqwin->icon((char *)flarq_icon_pixmap);
 #endif
 

src/include/fl_digi.h
@@ -135,7 +135,7 @@ extern std::string		main_window_title;
 extern void toggleRSID();
 
 extern void set_menus();
-extern void create_fl_digi_main();
+extern void create_fl_digi_main(int argc, char** argv);
 extern void update_main_title();
 extern void activate_rig_menu_item(bool b);
 extern void activate_test_menu_item(bool b);

src/include/flmisc.h
@@ -21,7 +21,7 @@ void adjust_color_inv(unsigned char& bg1r, unsigned char& bg1g, unsigned char& b
 
 #if !defined(__APPLE__) && !defined(__WOE32__)
 #  include <FL/x.H>
-void make_pixmap(Pixmap *xpm, const char **data);
+void make_pixmap(Pixmap *xpm, const char **data, int argc, char** argv);
 #endif
 
 class notify_dialog : public Fl_Window

src/main.cxx
@@ -156,6 +156,7 @@ FILE	*server;
 FILE	*client;
 bool	mailserver = false, mailclient = false, arqmode = false;
 static bool show_cpucheck = false;
+static bool iconified = false;
 
 RXMSGSTRUC rxmsgst;
 int		rxmsgid = -1;
@@ -313,7 +314,7 @@ int main(int argc, char ** argv)
 
 	Fl::scheme(progdefaults.ui_scheme.c_str());
 	progdefaults.initFonts();
-	create_fl_digi_main();
+	create_fl_digi_main(argc, argv);
 
 #if BENCHMARK_MODE
 	return setup_benchmark();
@@ -365,6 +366,9 @@ int main(int argc, char ** argv)
 	progStatus.initLastState();
 	fl_digi_main->show(argc, argv);
 #endif
+	if (iconified)
+		for (Fl_Window* w = Fl::first_window(); w; w = Fl::next_window(w))
+			w->iconize();
 	update_main_title();
 
 	arq_init();
@@ -579,6 +583,8 @@ int parse_args(int argc, char **argv, int& idx)
 		if (main_window_title.empty() && argc > idx &&
 		    (!strcmp(argv[idx], "-ti") || !strcmp(argv[idx], "-title")))
 			main_window_title = argv[idx + 1];
+		else if (!strcmp(argv[idx], "-i") || !strcmp(argv[idx], "-iconic"))
+			iconified = true;
 		return 0;
 	}
 

src/widgets/flmisc.cxx
@@ -89,13 +89,13 @@ void adjust_color_inv(unsigned char& bg1r, unsigned char& bg1g, unsigned char& b
 #  include <FL/Fl_Window.H>
 #  include <FL/Fl_Pixmap.H>
 #  include <FL/fl_draw.H>
-void make_pixmap(Pixmap *xpm, const char **data)
+void make_pixmap(Pixmap *xpm, const char **data, int argc, char** argv)
 {
 	// We need a displayed window to provide a GC for X_CreatePixmap
 	Fl_Window w(0, 0, PACKAGE_NAME);
 	w.xclass(PACKAGE_NAME);
 	w.border(0);
-	w.show();
+	w.show(argc, argv);
 
 	Fl_Pixmap icon(data);
 	int maxd = MAX(icon.w(), icon.h());

commit 872f52ccafa5764603f5f07dc199457d25c33131

WF_ONLY height fix
    Fix height bug for WF_only introduced by commit bb37cc1c6

src/dialogs/fl_digi.cxx
@@ -3942,13 +3942,14 @@ void altTabs()
 	tabsConfigure->remove(tabQRZ);
 }
 
+int WF_only_height = 0;
+
 void create_fl_digi_main_WF_only() {
 
 	int fnt = fl_font();
 	int fsize = fl_size();
 	int freqheight = Hentry + 2 * pad;
 	int Y = 0;
-	int nomH = 0;
 
 	fl_font(fnt, freqheight);
 	fl_font(fnt, fsize);
@@ -3957,9 +3958,9 @@ void create_fl_digi_main_WF_only() {
 	IMAGE_WIDTH = 4000;//progdefaults.HighFreqCutoff;
 	Hwfall = progdefaults.wfheight;
 	Wwfall = progStatus.mainW - 2 * DEFAULT_SW - 2 * pad;
-	nomH = Hmenu + Hwfall + Hstatus + 4 * pad;
+	WF_only_height = Hmenu + Hwfall + Hstatus + 4 * pad;
 
-	fl_digi_main = new Fl_Double_Window(progStatus.mainW, nomH);
+	fl_digi_main = new Fl_Double_Window(progStatus.mainW, WF_only_height);
 
 		mnuFrame = new Fl_Group(0,0,progStatus.mainW, Hmenu);
 
@@ -4160,7 +4161,10 @@ void create_fl_digi_main(int argc, char** argv)
 #endif
 
 	fl_digi_main->xclass(PACKAGE_NAME);
-	fl_digi_main->size_range(WMIN, HMIN);
+
+	fl_digi_main->size_range(
+		WMIN, bWF_only ? WF_only_height : HMIN,
+		0, bWF_only ? WF_only_height : 0);
 }
 
 void put_freq(double frequency)