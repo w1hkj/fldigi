3a51cef81a235a2fd98759fafc6de73883497e4c
David Freese <w1hkj@w1hkj.com>
2010-11-19 08:30:31 -0600

    QSO_DB sort order
      Corrected bug in date-time sorting of the logbook database.
      Sort is now by DATE and TIME vice DATE and TIME_OFF

--- src/logbook/qso_db.cxx
+++ src/logbook/qso_db.cxx
@@ -119,9 +119,9 @@ const cQsoRec &cQsoRec::operator=(const cQsoRec &right) {
 }
 
 int compareTimes (const cQsoRec &r1, const cQsoRec &r2) {
-	if (r1.qsofield[TIME_OFF] < r2.qsofield[TIME_OFF])
+	if (r1.qsofield[TIME_ON] < r2.qsofield[TIME_ON])
 		return -1;
-	if (r1.qsofield[TIME_OFF] > r2.qsofield[TIME_OFF])
+	if (r1.qsofield[TIME_ON] > r2.qsofield[TIME_ON])
 		return 1;
 	return 0;
 }
