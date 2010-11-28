w1hkj/fldigi

commit 8ae9ddd6f5573a045f1afcf4e4bb8395754b6cd3

DATE_TIME sorting
  * Added user selection for date/time ON or OFF sorting
  * Cabrillo & ADIF exports follow the ON / OFF selection
  * Logbook browser follows ON / OFF selection
  * Added user selection to force DATE/TIME ON == DATE/TIME OFF

src/logger/logger.cxx
@@ -51,8 +51,6 @@ using namespace std;
 
 //---------------------------------------------------------------------
 const char *logmode;
-char logdate[32];
-char logtime[32];
 
 static string log_msg;
 static string errmsg;
@@ -97,7 +95,7 @@ void submit_ADIF(cQsoRec &rec)
 {
 	adif.erase();
 	
-	putadif(QSO_DATE, rec.getField(QSO_DATE)); 
+	putadif(QSO_DATE, rec.getField(QSO_DATE_OFF));
 	putadif(TIME_ON, rec.getField(TIME_ON));
 	putadif(TIME_OFF, rec.getField(TIME_OFF));
 	putadif(CALL, rec.getField(CALL));
@@ -149,7 +147,7 @@ static void send_IPC_log(cQsoRec &rec)
 	log_msg = "";
 	log_msg = log_msg + "program:"	+ PACKAGE_NAME + " v " 	+ PACKAGE_VERSION + LOG_MSEPARATOR;
 	addtomsg("version:",	LOG_MVERSION);
-	addtomsg("date:",		logdate);
+	addtomsg("date:",		rec.getField(QSO_DATE_OFF));
 	addtomsg("time:", 		rec.getField(TIME_ON));
 	addtomsg("endtime:", 	rec.getField(TIME_OFF));
 	addtomsg("call:",		rec.getField(CALL));
@@ -202,15 +200,6 @@ void submit_log(void)
 {
 	if (progStatus.spot_log)
 		spot_log(inpCall->value(), inpLoc->value());
-
-	struct tm *tm;
-	time_t t;
-
-	time(&t);
-        tm = gmtime(&t);
-		strftime(logdate, sizeof(logdate), "%d %b %Y", tm);
-		strftime(logtime, sizeof(logtime), "%H%M", tm);
-	logmode = mode_info[active_modem->get_mode()].adif_name;
 
 	AddRecord();
 

src/dialogs/confdialog.cxx
@@ -191,6 +191,21 @@ static void cb_btnCallUpperCase(Fl_Check_Button* o, void*) {
 progdefaults.changed = true;
 }
 
+Fl_Check_Button *btnDateTimeSort=(Fl_Check_Button *)0;
+
+static void cb_btnDateTimeSort(Fl_Check_Button* o, void*) {
+  progdefaults.sort_date_time_off = o->value();
+progdefaults.changed = true;
+reload_browser();
+}
+
+Fl_Check_Button *btndate_time_force=(Fl_Check_Button *)0;
+
+static void cb_btndate_time_force(Fl_Check_Button* o, void*) {
+  progdefaults.force_date_time = o->value();
+progdefaults.changed = true;
+}
+
 Fl_Input2 *inpMyPower=(Fl_Input2 *)0;
 
 static void cb_inpMyPower(Fl_Input2* o, void*) {
@@ -2871,31 +2886,43 @@ ab and newline are automatically included."));
             { Fl_Group* o = new Fl_Group(8, 130, 484, 146, _("QSO logging"));
               o->box(FL_ENGRAVED_FRAME);
               o->align(FL_ALIGN_TOP_LEFT|FL_ALIGN_INSIDE);
-              { Fl_Check_Button* o = btnNagMe = new Fl_Check_Button(125, 139, 155, 20, _("Prompt to save log"));
+              { Fl_Check_Button* o = btnNagMe = new Fl_Check_Button(21, 158, 238, 20, _("Prompt to save log"));
                 btnNagMe->tooltip(_("Bug me about saving log entries"));
                 btnNagMe->down_box(FL_DOWN_BOX);
                 btnNagMe->callback((Fl_Callback*)cb_btnNagMe);
                 o->value(progdefaults.NagMe);
               } // Fl_Check_Button* btnNagMe
-              { Fl_Check_Button* o = btnClearOnSave = new Fl_Check_Button(125, 164, 125, 20, _("Clear on save"));
+              { Fl_Check_Button* o = btnClearOnSave = new Fl_Check_Button(21, 183, 238, 20, _("Clear on save"));
                 btnClearOnSave->tooltip(_("Clear log entries after saving or using macro <LOG>"));
                 btnClearOnSave->down_box(FL_DOWN_BOX);
                 btnClearOnSave->callback((Fl_Callback*)cb_btnClearOnSave);
                 o->value(progdefaults.ClearOnSave);
               } // Fl_Check_Button* btnClearOnSave
-              { Fl_Check_Button* o = btnAutoFillQSO = new Fl_Check_Button(125, 189, 225, 20, _("Auto-fill Country and Azimuth"));
+              { Fl_Check_Button* o = btnAutoFillQSO = new Fl_Check_Button(21, 208, 238, 20, _("Auto-fill Country and Azimuth"));
                 btnAutoFillQSO->tooltip(_("Fill in Country / Azimuth using cty.dat information"));
                 btnAutoFillQSO->down_box(FL_DOWN_BOX);
                 btnAutoFillQSO->callback((Fl_Callback*)cb_btnAutoFillQSO);
                 o->value(progdefaults.autofill_qso_fields);
               } // Fl_Check_Button* btnAutoFillQSO
-              { Fl_Check_Button* o = btnCallUpperCase = new Fl_Check_Button(125, 214, 282, 20, _("Convert callsign field to upper case"));
+              { Fl_Check_Button* o = btnCallUpperCase = new Fl_Check_Button(21, 233, 238, 20, _("Convert callsign to upper case"));
                 btnCallUpperCase->tooltip(_("Force callsign field to UPPERCASE"));
                 btnCallUpperCase->down_box(FL_DOWN_BOX);
                 btnCallUpperCase->callback((Fl_Callback*)cb_btnCallUpperCase);
                 o->value(progdefaults.calluppercase);
               } // Fl_Check_Button* btnCallUpperCase
-              { Fl_Input2* o = inpMyPower = new Fl_Input2(125, 240, 50, 24, _("Transmit Power"));
+              { Fl_Check_Button* o = btnDateTimeSort = new Fl_Check_Button(275, 183, 191, 20, _("Sort by Date/Time OFF"));
+                btnDateTimeSort->tooltip(_("Sort by date/time OFF - effects all ADIF/Cabrillo reports"));
+                btnDateTimeSort->down_box(FL_DOWN_BOX);
+                btnDateTimeSort->callback((Fl_Callback*)cb_btnDateTimeSort);
+                o->value(progdefaults.sort_date_time_off);
+              } // Fl_Check_Button* btnDateTimeSort
+              { Fl_Check_Button* o = btndate_time_force = new Fl_Check_Button(275, 208, 191, 20, _("Date time ON == OFF"));
+                btndate_time_force->tooltip(_("Force date/time ON == date/time OFF"));
+                btndate_time_force->down_box(FL_DOWN_BOX);
+                btndate_time_force->callback((Fl_Callback*)cb_btndate_time_force);
+                o->value(progdefaults.force_date_time);
+              } // Fl_Check_Button* btndate_time_force
+              { Fl_Input2* o = inpMyPower = new Fl_Input2(275, 154, 50, 24, _("Transmit Power"));
                 inpMyPower->tooltip(_("Tx power used for logbook entries"));
                 inpMyPower->box(FL_DOWN_BOX);
                 inpMyPower->color((Fl_Color)FL_BACKGROUND2_COLOR);

src/dialogs/confdialog.fl
@@ -181,7 +181,7 @@ progdefaults.changed = true;}
         }
       }
       Fl_Group tabUI {
-        label UI
+        label UI open
         xywh {-3 25 508 345} hide
       } {
         Fl_Tabs tabsUI {open
@@ -268,35 +268,50 @@ defined here. Tab and newline are automatically included.} xywh {125 285 75 24} 
                 label {Prompt to save log}
                 callback {progdefaults.NagMe=o->value();
 progdefaults.changed = true;}
-                tooltip {Bug me about saving log entries} xywh {125 139 155 20} down_box DOWN_BOX
+                tooltip {Bug me about saving log entries} xywh {21 158 238 20} down_box DOWN_BOX
                 code0 {o->value(progdefaults.NagMe);}
               }
               Fl_Check_Button btnClearOnSave {
                 label {Clear on save}
                 callback {progdefaults.ClearOnSave=o->value();
 progdefaults.changed = true;}
-                tooltip {Clear log entries after saving or using macro <LOG>} xywh {125 164 125 20} down_box DOWN_BOX
+                tooltip {Clear log entries after saving or using macro <LOG>} xywh {21 183 238 20} down_box DOWN_BOX
                 code0 {o->value(progdefaults.ClearOnSave);}
               }
               Fl_Check_Button btnAutoFillQSO {
                 label {Auto-fill Country and Azimuth}
                 callback {progdefaults.autofill_qso_fields = o->value();
 progdefaults.changed = true;}
-                tooltip {Fill in Country / Azimuth using cty.dat information} xywh {125 189 225 20} down_box DOWN_BOX
+                tooltip {Fill in Country / Azimuth using cty.dat information} xywh {21 208 238 20} down_box DOWN_BOX
                 code0 {o->value(progdefaults.autofill_qso_fields);}
               }
               Fl_Check_Button btnCallUpperCase {
-                label {Convert callsign field to upper case}
+                label {Convert callsign to upper case}
                 callback {progdefaults.calluppercase = o->value();
 progdefaults.changed = true;}
-                tooltip {Force callsign field to UPPERCASE} xywh {125 214 282 20} down_box DOWN_BOX
+                tooltip {Force callsign field to UPPERCASE} xywh {21 233 238 20} down_box DOWN_BOX
                 code0 {o->value(progdefaults.calluppercase);}
+              }
+              Fl_Check_Button btnDateTimeSort {
+                label {Sort by Date/Time OFF}
+                callback {progdefaults.sort_date_time_off = o->value();
+progdefaults.changed = true;
+reload_browser();}
+                tooltip {Sort by date/time OFF - effects all ADIF/Cabrillo reports} xywh {275 183 191 20} down_box DOWN_BOX
+                code0 {o->value(progdefaults.sort_date_time_off);}
+              }
+              Fl_Check_Button btndate_time_force {
+                label {Date time ON == OFF}
+                callback {progdefaults.force_date_time = o->value();
+progdefaults.changed = true;}
+                tooltip {Force date/time ON == date/time OFF} xywh {275 208 191 20} down_box DOWN_BOX
+                code0 {o->value(progdefaults.force_date_time);}
               }
               Fl_Input inpMyPower {
                 label {Transmit Power}
                 callback {progdefaults.mytxpower = o->value();
 progdefaults.changed = true;}
-                tooltip {Tx power used for logbook entries} xywh {125 240 50 24} align 8
+                tooltip {Tx power used for logbook entries} xywh {275 154 50 24} align 8
                 code0 {o->value(progdefaults.mytxpower.c_str());}
                 code1 {o->labelsize(FL_NORMAL_SIZE);}
                 class Fl_Input2
@@ -2991,7 +3006,7 @@ progdefaults.changed = true;}
         }
       }
       Fl_Group tabMisc {
-        label Misc open
+        label Misc
         xywh {0 25 500 345} hide
       } {
         Fl_Tabs tabsMisc {open

src/include/confdialog.h
@@ -43,6 +43,8 @@ extern Fl_Check_Button *btnNagMe;
 extern Fl_Check_Button *btnClearOnSave;
 extern Fl_Check_Button *btnAutoFillQSO;
 extern Fl_Check_Button *btnCallUpperCase;
+extern Fl_Check_Button *btnDateTimeSort;
+extern Fl_Check_Button *btndate_time_force;
 extern Fl_Input2 *inpMyPower;
 extern Fl_Group *tabContest;
 #include <FL/Fl_Box.H>

src/include/configuration.h
@@ -567,6 +567,12 @@
         ELEM_(bool, ClearOnSave, "CLEARONSAVE",                                         \
               "Clear log fields on save",                                               \
               false)                                                                    \
+        ELEM_(bool, sort_date_time_off, "SORTDATEOFF",                                  \
+              "Sort log by date/time off",                                              \
+              true)                                                                     \
+        ELEM_(bool, force_date_time, "FORCEDATETIME",                                   \
+              "Force date/time ON == OFF",                                              \
+              false)                                                                    \
         ELEM_(bool, menuicons, "MENUICONS",                                             \
               "Show menu icons",                                                        \
               true)                                                                     \

src/include/logsupport.h
@@ -55,6 +55,7 @@ extern cQsoRec* SearchLog(const char *callsign);
 extern void DupCheck();
 extern void cb_search(Fl_Widget* w, void*);
 extern int log_search_handler(int);
+extern void reload_browser();
 
 extern void cb_doExport();
 

src/include/qso_db.h
@@ -80,7 +80,7 @@ public:
 	bool qsoIsValidFile(const char *);
 	int qsoReadFile (const char *);
 	int qsoWriteFile (const char *);
-	void SortByDate();
+	void SortByDate(bool);
 	void SortByCall ();
 	void SortByMode ();
 	void SortByFreq ();

src/logbook/adif_io.cxx
@@ -9,6 +9,7 @@
 
 #include "adif_io.h"
 #include "config.h"
+#include "configuration.h"
 #include "lgbook.h"
 #include "icons.h"
 #include "gettext.h"
@@ -239,7 +240,7 @@ void cAdifIO::readFile (const char *fname, cQsoDb *db) {
 	}
 
 	log_checksum = file_checksum;
-	db->SortByDate();
+	db->SortByDate(progdefaults.sort_date_time_off);
 	delete [] buff;
 }
 
@@ -271,7 +272,7 @@ int cAdifIO::writeFile (const char *fname, cQsoDb *db)
 			 strlen(ADIF_VERS), ADIF_VERS,
 			 strlen(PACKAGE_NAME), PACKAGE_NAME,
 			 strlen(PACKAGE_VERSION), PACKAGE_VERSION);
-//	db->SortByDate();
+
 	for (int i = 0; i < db->nbrRecs(); i++) {
 		rec = db->getRec(i);
 		if (rec->getField(EXPORT)[0] == 'E') {

commit 734ddfd46f4ee855abd6951e85e5489e760d1356

MODE logging
  Fixed bug introduced by 8ae9ddd6f5573a045f1afcf4e4bb8395754b6cd3

src/logger/logger.cxx
@@ -201,6 +201,7 @@ void submit_log(void)
 	if (progStatus.spot_log)
 		spot_log(inpCall->value(), inpLoc->value());
 
+	logmode = mode_info[active_modem->get_mode()].adif_name;
 	AddRecord();
 
 }