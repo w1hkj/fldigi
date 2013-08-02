607a696bac57689cdf33cbd87ac3754f08b8e10f
David Freese <w1hkj@w1hkj.com>
2013-08-02 14:52:14 -0500

    Missing header
      * Some builds reported missing header

--- src/misc/coordinate.cxx
+++ src/misc/coordinate.cxx
@@ -20,6 +20,8 @@
 // along with fldigi.  If not, see <http://www.gnu.org/licenses/>.
 // ----------------------------------------------------------------------------
 
+#include <stdio.h>
+
 #include <math.h>
 #include <string.h>
 #include <stdexcept>
