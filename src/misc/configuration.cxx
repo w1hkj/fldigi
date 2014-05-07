ba246ebad0f57913753026965ec753de5ef8d403
David Freese <w1hkj@w1hkj.com>
2014-05-07 07:33:23 -0500

    Virtual ports
      * Added /opt/vttySn usage for Linux
        - create a linked name to a virtual port (socat)
          with link defined as /opt/vttyS0 ...

--- src/misc/configuration.cxx
+++ src/misc/configuration.cxx
@@ -866,7 +866,8 @@ void configuration::testCommPorts()
 		"/dev/ttyUSB%u",
 		"/dev/usb/ttyUSB%u",
 		"/dev/ttyACM%u",
-		"/dev/usb/ttyACM%u"
+		"/dev/usb/ttyACM%u",
+		"/opt/vttyS%u"
 #elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
 		"/dev/tty%2.2u"
 #elif defined(__CYGWIN__)
