bef440589b8c098f6305c62d093a5f9056c8eb3d
David Freese <w1hkj@w1hkj.com>
2014-09-25 06:59:39 -0500

    Power meter
      * Added Power meter class display widget
        - scales of 25, 50, 100, 200 W
        - autoscale can be selected

---
+++ src/include/pwrmeter.h
@@ -0,0 +1,87 @@
+//
+// pwrmeter.h
+//
+//  PWRmeter bar widget routines.
+// ----------------------------------------------------------------------------
+// Copyright (C) 2014
+//              David Freese, W1HKJ
+//
+// This file is part of fldigi
+//
+// fldigi is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// fldigi is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// ----------------------------------------------------------------------------
+
+
+#ifndef PWRMETER
+#define PWRMETER
+
+//
+// Include necessary headers.
+//
+
+#include <FL/Fl.H>
+#include <FL/Fl_Widget.H>
+
+//
+// PWRmeter class...
+//
+
+class PWRmeter : public Fl_Widget
+{
+public:
+enum {P25, P50, P100, P200, AUTO};
+
+private:
+	double	value_,
+			maximum_;
+	int		sval;			// Size of sval bar...
+	int		bx, by, bw, bh;	// Box areas...
+	int		tx, tw;			// Temporary X + width
+	int		ty, th;			// Temporary Y + height
+	int		sx;				// meter left offset
+	int		meter_width;
+	int		meter_height;
+	int		select_;
+	Fl_Color bgnd_;
+	Fl_Color fgnd_;
+	Fl_Color scale_color;
+	static const char *W25_face;
+	static const char *W50_face;
+	static const char *W100_face;
+	static const char *W200_face;
+
+protected:
+	virtual void draw();
+
+public:
+	PWRmeter(int x, int y, int w, int h, const char *l = 0);
+
+	void	value(double v) { value_ = v; redraw(); }
+	double	value() const { return (value_); }
+
+	void	set_background(Fl_Color c1) { bgnd_ = c1; redraw(); }
+	void	set_metercolor(Fl_Color c2) { fgnd_ = c2; redraw(); }
+	void	set_scalecolor(Fl_Color c3) { scale_color = c3; redraw(); }
+
+	void	select(int sel);
+private:
+	void	select_auto();
+	void	select_25W();
+	void	select_50W();
+	void	select_100W();
+	void	select_200W();
+};
+
+#endif // !pwrmeter
+
---
+++ src/widgets/pwrmeter.cxx
@@ -0,0 +1,193 @@
+//
+// pwrmeter.cxx
+//
+// PWRmeter bar widget routines.
+//
+// A part of the fldigi.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// ----------------------------------------------------------------------------
+
+#include <config.h>
+#include <cmath>
+
+#include <FL/Fl.H>
+#include <FL/fl_draw.H>
+
+#include "pwrmeter.h"
+
+//
+// pwrmeter is a pwrmeter bar widget based off Fl_Widget that shows a
+// standard pwrmeter bar in horizontal format
+
+void PWRmeter::draw()
+{
+	if (select_ == 4) select_auto();
+
+	sval = round(meter_width * (value_) / (maximum_));
+	if (sval > meter_width) sval = meter_width;
+	if (sval < 0) sval = 0;
+
+// Draw the box and label...
+	draw_box();
+	draw_box(box(), tx, ty, tw, th, bgnd_);
+	if (sval > 0)
+		draw_box(FL_FLAT_BOX,
+			tx + sx, ty + 2,
+			sval, 
+			th - 4,
+			fgnd_);
+	labelcolor(scale_color);
+	draw_label();
+}
+
+void PWRmeter::select_25W()
+{
+	maximum_ = 25;
+	label(W25_face);
+	fl_font(FL_HELVETICA, labelsize());
+	meter_width = fl_width(W25_face);
+	sx = (tw - meter_width) / 2 + fl_width("|") / 2;
+	meter_width -= fl_width("|  25");
+}
+
+void PWRmeter::select_50W()
+{
+	maximum_ = 50;
+	label(W50_face);
+	fl_font(FL_HELVETICA, labelsize());
+	meter_width = fl_width(W50_face);
+	sx = (tw - meter_width) / 2 + fl_width("|") / 2;
+	meter_width -= fl_width("|  50");
+}
+
+void PWRmeter::select_100W()
+{
+	maximum_ = 100;
+	label(W100_face);
+	fl_font(FL_HELVETICA, labelsize());
+	meter_width = fl_width(W100_face);
+	sx = (tw - meter_width) / 2 + fl_width("|") / 2;
+	meter_width -= fl_width("| 100");
+}
+
+void PWRmeter::select_200W()
+{
+	maximum_ = 200;
+	label(W200_face);
+	fl_font(FL_HELVETICA, labelsize());
+	meter_width = fl_width(W200_face);
+	sx = (tw - meter_width) / 2 + fl_width("|") / 2;
+	meter_width -= fl_width("| 200");
+}
+
+void PWRmeter::select_auto()
+{
+	if (value_ <= 25.0) {
+		select_25W();
+	} else if (value_ <= 50.0) {
+		select_50W();
+	} else if (value_ <= 100) {
+		select_100W();
+	} else {
+		select_200W();
+	}
+	redraw();
+}
+
+void PWRmeter::select( int sel ) { 
+	switch (sel) {
+		case P25:
+			select_25W();
+			select_ = 0;
+			break;
+		case P50:
+			select_50W();
+			select_ = 1;
+			break;
+		case P100:
+			select_100W();
+			select_ = 2;
+			break;
+		case P200:
+			select_200W();
+			select_ = 3;
+			break;
+		case AUTO:
+		default:
+			select_auto();
+			select_ = 4;
+	}
+	redraw();
+}
+
+const char * PWRmeter::W25_face   = "| : : : : | : : : : | : : : : | : : : : | : : : : |  25";
+const char * PWRmeter::W50_face   = "|    :    |    :    |    :    |    :    |    :    |  50";
+const char * PWRmeter::W100_face  = "|    |    |    |    |    |    |    |    |    |    | 100";
+const char * PWRmeter::W200_face  = "|     :     |     :      |      :     |     :     | 200";
+
+PWRmeter::PWRmeter(int X, int Y, int W, int H, const char* l)
+: Fl_Widget(X, Y, W, H, "")
+{
+	align(FL_ALIGN_INSIDE);
+	box(FL_DOWN_BOX);
+	bgnd_ = FL_BACKGROUND2_COLOR;
+	fgnd_ = FL_GREEN;
+	scale_color = FL_BLACK;
+
+	maximum_ = 100.0;
+	value_ = 0.0;
+
+	select_ = 2; // 100 W scale
+
+  // Get the box borders...
+	bx = Fl::box_dx(box());
+	by = Fl::box_dy(box());
+	bw = Fl::box_dw(box());
+	bh = Fl::box_dh(box());
+
+	tx = X + bx;
+	tw = W - bw;
+	ty = Y + by;
+	th = H - bh;
+
+	static int fsize = 6;
+	fl_font(FL_HELVETICA, fsize);
+	meter_width = fl_width(W100_face);
+	while ((meter_width < tw) && (fl_height() < th)) {
+		fsize++;
+		fl_font(FL_HELVETICA, fsize);
+		meter_width = fl_width(W100_face);
+	}
+	fsize--;
+	fl_font(FL_HELVETICA, fsize);
+	meter_width = fl_width(W100_face);
+
+//printf("widths: 25W %.0f, 50W %.0f, 100W %.0f, 200W %.0f\n",
+//fl_width(W25_face), fl_width(W50_face), fl_width(W100_face), fl_width(W200_face));
+
+	meter_height = fl_height();
+	label(W100_face);
+	labelfont(FL_HELVETICA);
+	labelsize(fsize);
+	labelcolor(scale_color);
+
+	sx = (tw - meter_width) / 2 + fl_width("|") / 2;
+
+	meter_width -= fl_width("| 100");
+}
+
+//
+// End of PWRmeter.cxx
+//
