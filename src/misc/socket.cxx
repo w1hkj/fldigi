f40e79d7c671d82254d81f79daa1c720e07cda70
David Freese <iam_w1hkj@w1hkj.com>
2017-03-28 07:44:05 -0500

    Socket connect
      * FreeBSD mod causes seg fault on non BSD platforms
        - make change OS dependent

--- src/misc/socket.cxx
+++ src/misc/socket.cxx
@@ -607,7 +607,11 @@ void Socket::open(const Address& addr)
 	address = addr;
 	size_t n = address.size();
 
+#ifdef __FreeBSD__
 	for (anum = n-1; anum >= 1; anum--) {
+#else
+	for (anum = n-1; anum >= 0; anum--) {
+#endif
 		ainfo = address.get(anum);
 		LOG_INFO("Trying %s", address.get_str(ainfo).c_str());
 		if ((sockfd = socket(ainfo->ai_family, ainfo->ai_socktype, ainfo->ai_protocol)) != -1)
