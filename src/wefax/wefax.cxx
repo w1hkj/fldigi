15491930f0d79df635bc828ce915fc9bc5692c1f
David Freese <iam_w1hkj@w1hkj.com>
2017-04-11 15:51:18 -0500

    Revert "wefax"
      * This reverts commit 5609415222335e9376defc325f20ae29c5c8c8ca.
      * Modifies wefax.cxx iaw request from Remi, wefax author

--- src/wefax/wefax.cxx
+++ src/wefax/wefax.cxx
@@ -556,8 +556,7 @@ private:
 			= wf->powerDensity(m_carrier - 2 * m_apt_start_freq, bandwidth_apt_start)
 			+ wf->powerDensity(m_carrier -     m_apt_start_freq, bandwidth_apt_start)
 			+ wf->powerDensity(m_carrier                       , bandwidth_apt_start)
-			+ wf->powerDensity(m_carrier +     m_apt_start_freq, bandwidth_apt_start)
-			+ wf->powerDensity(m_carrier + 2 * m_apt_start_freq, bandwidth_apt_start);
+			+ wf->powerDensity(m_carrier +     m_apt_start_freq, bandwidth_apt_start);
 
        		return decayavg( avg_pwr, pwr, 10 );
 	}
