51e2829d223c74d82552ed8ef702d43aa14cfb0a
David Freese <w1hkj@bellsouth.net>
2018-07-21 02:43:12 -0500

    Hamlib bug fix
      * submitted by Darrin, KA2ZLZ
      * rig_cmp function being passed to std::sort in
        src/rigcontrol/hamlib.cxx was returning true if
        the items are equal

--- src/rigcontrol/hamlib.cxx
+++ src/rigcontrol/hamlib.cxx
@@ -582,10 +582,10 @@ static bool rig_cmp(const struct rig_caps* rig1, const struct rig_caps* rig2)
 	if (ret < 0) return true;
 	ret = strcasecmp(rig1->model_name, rig2->model_name);
 	if (ret > 0) return false;
-	if (ret <= 0) return true;
-	if (rig1->rig_model > rig2->rig_model)
-		return false;
-	return true;
+	if (ret < 0) return true;
+	if (rig1->rig_model < rig2->rig_model)
+		return true;
+	return false;
 }
 
 void hamlib_get_rigs(void)
