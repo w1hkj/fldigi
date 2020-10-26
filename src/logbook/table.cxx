54e3167103b5c278fd4b0dd4faa7cbaac99b8a9c
David Freese <w1hkj@bellsouth.net>
2020-10-26 09:57:02 -0500

    FreeBSD/nullptr
      * nullptr is defined in _null.h

--- src/logbook/table.cxx
+++ src/logbook/table.cxx
@@ -46,8 +46,10 @@ using namespace std;
  * nullptr is not available in gcc < 4.6
  * Redefine to NULL for CentOS 6 and OSX Darwin
  */
-#ifndef nullptr
-#define nullptr NULL
+#ifndef __FreeBSD__
+ #ifndef nullptr
+ #define nullptr NULL
+ #endif
 #endif
 
 
