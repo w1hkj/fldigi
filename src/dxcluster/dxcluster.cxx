f2180d00a5f8317ad2d3bb251a8844eb97cab3e5
dave-w1hkj <w1hkj@bellsouth.net>
2021-03-06 07:46:37 -0600

    dxcluster
      * Immediate cmd
        . ! character as first in command definition will cause
          the command to be send immediate upon selection of that
          command button

--- src/dxcluster/dxcluster.cxx
+++ src/dxcluster/dxcluster.cxx
@@ -749,6 +749,7 @@ void DXcluster_submit()
 	try {
 		guard_lock dxc_lock(&DXcluster_mutex);
 		string sendbuf = trim(inp_dxcluster_cmd->value());
+		if (sendbuf[0] == '!') sendbuf.erase(0,1);
 		string test = ucasestr(sendbuf);
 		if (test.find("BYE") != string::npos) {
 			fl_alert2("Uncheck the \"Connect\" button to disconnect!");
@@ -1456,39 +1457,47 @@ void dxcluster_servers(Fl_Button*, void*)
 void dxc_click_m1(Fl_Button*, void*)
 {
 	inp_dxcluster_cmd->value(progdefaults.dxcm_text_1.c_str());
+	if (progdefaults.dxcm_text_1[0] == '!') DXcluster_submit();
 }
 
 void dxc_click_m2(Fl_Button*, void*)
 {
 	inp_dxcluster_cmd->value(progdefaults.dxcm_text_2.c_str());
+	if (progdefaults.dxcm_text_2[0] == '!') DXcluster_submit();
 }
 
 void dxc_click_m3(Fl_Button*, void*)
 {
 	inp_dxcluster_cmd->value(progdefaults.dxcm_text_3.c_str());
+	if (progdefaults.dxcm_text_3[0] == '!') DXcluster_submit();
 }
 
 void dxc_click_m4(Fl_Button*, void*)
 {
 	inp_dxcluster_cmd->value(progdefaults.dxcm_text_4.c_str());
+	if (progdefaults.dxcm_text_4[0] == '!') DXcluster_submit();
 }
 
 void dxc_click_m5(Fl_Button*, void*)
 {
 	inp_dxcluster_cmd->value(progdefaults.dxcm_text_5.c_str());
+	if (progdefaults.dxcm_text_5[0] == '!') DXcluster_submit();
 }
 
 void dxc_click_m6(Fl_Button*, void*)
 {
 	inp_dxcluster_cmd->value(progdefaults.dxcm_text_6.c_str());
+	if (progdefaults.dxcm_text_6[0] == '!') DXcluster_submit();
 }
 
 void dxc_click_m7(Fl_Button*, void*)
 {
 	inp_dxcluster_cmd->value(progdefaults.dxcm_text_7.c_str());
+	if (progdefaults.dxcm_text_7[0] == '!') DXcluster_submit();
 }
 
 void dxc_click_m8(Fl_Button*, void*)
 {
 	inp_dxcluster_cmd->value(progdefaults.dxcm_text_8.c_str());
+	if (progdefaults.dxcm_text_8[0] == '!') DXcluster_submit();
 }
