w1hkj/fldigi

commit 9c035e809ba51b24b6637026ab4ecac6b7daafde

cwid
  * enable cwid for
    . T/R button reset to Rx
    . ^r in xmlrpc transmit text strings
    . ^r in macro text
    . ^r in transmit text
    NOTE: recommend disabling cwid for all ARQ type
    message transmissions; flarq / flmsg.

src/dialogs/fl_digi.cxx
@@ -8961,46 +8961,6 @@ int get_tx_char(void)
 	if (Qidle_time) { return GET_TX_CHAR_NODATA; }
 	if (macro_idle_on) { return GET_TX_CHAR_NODATA; }
 
-	if (!macrochar.empty()) {
-		int ch = macrochar[0];
-		macrochar.erase(0,1);
-		start_deadman();
-		return ch;
-	}
-
-	if (xmltest_char_available) {
-		num_cps_chars++;
-		start_deadman();
-		return xmltest_char();
-	}
-
-	if (data_io_enabled == ARQ_IO && arq_text_available) {
-		start_deadman();
-		return arq_get_char();
-	} else if (data_io_enabled == KISS_IO && kiss_text_available) {
-		start_deadman();
-		return kiss_get_char();
-	}
-
-	if (active_modem == cw_modem && progdefaults.QSKadjust) {
-		start_deadman();
-		return szTestChar[2 * progdefaults.TestChar];
-	}
-
-	if ( (progStatus.repeatMacro > -1) && progStatus.repeatIdleTime > 0 &&
-		 !idling ) {
-		Fl::add_timeout(progStatus.repeatIdleTime, get_tx_char_idle);
-		idling = true;
-		return GET_TX_CHAR_NODATA;
-	}
-
-	int c;
-
-	if ((c = tx_encoder.pop()) != -1) {
-		start_deadman();
-		return(c);
-	}
-
 	if ((progStatus.repeatMacro > -1) && text2repeat.length()) {
 		string repeat_content;
 		int utf8size = fl_utf8len1(text2repeat[repeatchar]);
@@ -9016,13 +8976,57 @@ int get_tx_char(void)
 		goto transmit;
 	}
 
-	if (active_modem->get_mode() == MODE_IFKP)
+	int c;
+
+	if (!macrochar.empty()) {
+		c = macrochar[0];
+		macrochar.erase(0,1);
+		start_deadman();
+		return c;
+	}
+
+	if (data_io_enabled == ARQ_IO && arq_text_available) {
+		start_deadman();
+		c = arq_get_char();
+		return c;
+	} else if (data_io_enabled == KISS_IO && kiss_text_available) {
+		start_deadman();
+		c = kiss_get_char();
+		return c;
+	}
+
+	if (active_modem == cw_modem && progdefaults.QSKadjust) {
+		start_deadman();
+		c = szTestChar[2 * progdefaults.TestChar];
+		return c;
+	}
+
+	if ( (progStatus.repeatMacro > -1) && progStatus.repeatIdleTime > 0 &&
+		 !idling ) {
+		Fl::add_timeout(progStatus.repeatIdleTime, get_tx_char_idle);
+		idling = true;
+		return GET_TX_CHAR_NODATA;
+	}
+
+	if ((c = tx_encoder.pop()) != -1) {
+		start_deadman();
+		return(c);
+	}
+
+	if (xmltest_char_available) {
+		num_cps_chars++;
+		start_deadman();
+		c = xmltest_char();
+	}
+	else if (active_modem->get_mode() == MODE_IFKP) {
 		c = ifkp_tx_text->nextChar();
-	else 
-		if ((c = next_buffered_macro_char()) == 0) // preference given to buffered macro chars
-			c = TransmitText->nextChar();
+	}
+	else if ((c = next_buffered_macro_char()) == 0) { // preference given to buffered macro chars
+		c = TransmitText->nextChar();
+	}
 
 	if (c == GET_TX_CHAR_ETX) {
+		active_modem->set_CW_EOT();
 		return c;
 	}
 

src/rigcontrol/xmlrpc_rig.cxx
@@ -811,7 +811,6 @@ void set_flrig_notch()
 	XmlRpcValue val, result;
 	try {
 		val = (int)notch_frequency;
-std::cout << "set notch " << val << std::endl;
 		bool ret;
 		{
 			guard_lock flrig_lock(&mutex_flrig);
@@ -863,7 +862,6 @@ static void xmlrpc_rig_set_smeter(void *data)
 			smeter->show();
 		}
 		int val = reinterpret_cast<intptr_t>(data);
-LOG_VERBOSE("xmlrpc_rig_set_smeter: %d", val);
 		smeter->value(val);
 	}
 }

commit 5dd676f6bbcac1659d6dd53945de83636c02957e

Correct errors caused by commit 9c035e809b
  * original commit failed to correctly send flmsg autosend
    character stream

src/dialogs/fl_digi.cxx
@@ -9075,6 +9075,12 @@ int get_tx_char(void)
 		return c;
 	}
 
+	if (xmltest_char_available) {
+		num_cps_chars++;
+		start_deadman();
+		return xmltest_char();
+	}
+
 	if (data_io_enabled == ARQ_IO && arq_text_available) {
 		start_deadman();
 		c = arq_get_char();
@@ -9103,6 +9109,21 @@ int get_tx_char(void)
 		return(c);
 	}
 
+	if ((progStatus.repeatMacro > -1) && text2repeat.length()) {
+		std::string repeat_content;
+		int utf8size = fl_utf8len1(text2repeat[repeatchar]);
+		for (int i = 0; i < utf8size; i++)
+			repeat_content += text2repeat[repeatchar + i];
+		repeatchar += utf8size;
+		tx_encoder.push(repeat_content);
+
+		if (repeatchar >= text2repeat.length()) {
+			text2repeat.clear();
+			macros.repeat(progStatus.repeatMacro);
+		}
+		goto transmit;
+	}
+
 	disable_lowercase = false;
 	if (xmltest_char_available) {
 		num_cps_chars++;
@@ -9110,15 +9131,12 @@ int get_tx_char(void)
 		c = xmltest_char();
 		disable_lowercase = true;
 	}
-	else if (active_modem->get_mode() == MODE_IFKP) {
+	else if (active_modem->get_mode() == MODE_IFKP)
 		c = ifkp_tx_text->nextChar();
-	}
-	else if ((c = next_buffered_macro_char()) == 0) { // preference given to buffered macro chars
+	else if ((c = next_buffered_macro_char()) == 0) // preference given to buffered macro chars
 		c = TransmitText->nextChar();
-	}
 
 	if (c == GET_TX_CHAR_ETX) {
-		active_modem->set_CW_EOT();
 		return c;
 	}
 